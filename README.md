920e0e66ae64ed6cca22512b731df15e146bf420
interactafraz <interact@afraz.de>
2023-05-14 22:08:02 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# anylog
+# AnyLog
 AnyLog is a lightweight tool to easily log values (like texts or numbers) in a JSON file using your own web server.
+
+It was developed for automated processes where different devices, apps and operating systems need to exchange data with each other in a simple and fast way. This includes workflow tools like [n8n](https://github.com/n8n-io/n8n) as well as iOS Shortcuts.
+
+### Possible use cases
+* Checkin at work using iOS shortcut, use AnyLog to log user and office location, create statistics based on log entries and save them to Google Sheet with n8n
+* As backlog: Take supplies from your food storage, use AnyLog to log what you've taken, add new items to specific grocery lists via n8n
+
+## Features
+* Save keys and values (via POST method)
+* Automatically assign GUID to each log entry
+* Delete individual log entries
+* Limit maximum log entries
+* Preview log entries in browser
+
+## Prerequisites
+* Common web server with PHP support
+
+## Installation
+* Copy files to web server
+
+## Usage
+### Save data
+The script expects POST data in the following format
+
+    {"meal": "Frozen Fries","quantity": "900"}
+
+### Delete data
+If you're using AnyLog for backlog purposes it might be useful to delete certain entries after they have been processed in your system. To do this you need the GUID which gets assigned to every log entry when it's created.
+
+* To delete an entry use `index.php?delete=[GUID]`
+
+## License
+tba
---
+++ index.php
@@ -0,0 +1,158 @@
+<?php 
+
+$file = "log.json";
+$entryLimit = 100;
+$previewTimeZone = "Europe/Berlin";//Only for preview output
+
+$logData = array();
+function addToLog($sentData,$fp) {
+	global $file;
+	global $logData;
+	global $entryLimit;	
+	$guid = rand(); //Generate Random Number
+	
+	$entry = array();
+	$timestamp = time();
+	
+	$entry['time'] = $timestamp;
+	$entry['guid'] = $guid;
+	
+	foreach ($sentData as $key => $value) {
+		$entry[$key] = $value;
+	}
+	
+	$logData = array_reverse($logData); //Reverse array to put recent entries to end
+	array_push($logData, $entry); //Add to existing entries
+	
+	if( count($logData) > $entryLimit){ //If more entries than allowed
+		$logData = array_reverse($logData); //Reverse array to maintain order of recent entries	
+		$logDataFiltered = array();
+	
+		$counter = 0;
+		foreach ($logData as $entry) {
+			if ($counter == $entryLimit){
+				break;
+			}
+			$counter = $counter + 1;
+			array_push($logDataFiltered, $entry); //Add to filtered entries
+		}
+		
+		$logData = $logDataFiltered;
+		$logData = array_reverse($logData); //Reverse array to maintain order of recent entries	
+	}
+	
+	$logData = array_reverse($logData); //Reverse array to keep recent entries in the first place
+	
+	rewind($fp);
+	ftruncate($fp, 0);
+	fwrite($fp, json_encode($logData)); //Save new data to file
+}
+
+
+
+if ( file_exists($file) ) { //Check if data exists
+
+	$fp = fopen($file, 'r+');
+	flock($fp, LOCK_EX); //Lock file to avoid other processes writing to it simlutanously 
+	
+	$jsonData = stream_get_contents($fp);
+	if($jsonData == ""){
+		echo "Could not read Log file (maybe empty)";
+		die;
+	}
+	$logData = json_decode($jsonData, true);
+	
+	if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty(json_decode($_POST['data'], true))) { //If data received via POST
+		
+		//Debug
+		//$debug = fopen('debug.txt', 'w');
+		//fwrite($debug, $_POST['data']); //Save new data to file
+		//fclose($debug);
+		//Debug
+		
+		addToLog( json_decode($_POST['data'], true),$fp );
+	}
+	elseif (!empty($_GET)) { //If data received via GET
+		if ( isset($_GET['delete']) ){ //If specific entry should be deleted
+			if (empty($logData)) {
+				echo "Could not read Log file (maybe empty)";
+				die;
+			}
+			
+			$counter = 0;
+			$targetFound = false;
+			
+			foreach ($logData as $entryExisting) { //Get every entry
+				foreach ($entryExisting as $key => $value) { //Get key and timestamp values
+				
+					if ($key == 'guid' && $value == $_GET['delete']){ //If target key has been found
+						unset($logData[$counter]);//Remove entry from logData
+						$targetFound = true;
+						break 2;
+					}
+					
+				}
+				$counter = $counter + 1;
+			}
+			
+			if($targetFound == true){
+				$logData = array_values($logData); //Rebuild index
+				
+				rewind($fp);
+				ftruncate($fp, 0);
+				fwrite($fp, json_encode($logData)); //Save new data to file
+				
+				echo 'Deleted';
+			}
+			else{
+				echo "Entry not found (maybe already deleted?)";
+			}		
+			
+		}
+	}
+
+	
+	flock($fp, LOCK_UN); //Unlock file for further access
+	fclose($fp);	
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>AnyLog</title>
+	</head>
+	<body>
+		<?php 
+			if ( empty($_GET) && file_exists($file) ) { //Check if data exists
+				foreach ($logData as $index => $entry) {
+					$elementCount = count($entry);
+					$counter = 0;
+					
+					foreach ($entry as $key => $value) {
+						$counter = $counter + 1;
+						
+						if($key == 'time'){
+							$time = DateTime::createFromFormat('U', $value)->setTimezone(new DateTimeZone($previewTimeZone))->format('d.m.Y \u\m H:i \U\h\r'); //Convert to datetime object							
+							echo $time . ' - ';
+						}
+						else{
+							echo $key . ' = ' . $value;
+						}
+						
+						
+						if( $counter != $elementCount && $key != 'time'){
+							echo ' | ';
+						}
+						
+					}
+					
+					echo "<br>";
+				}
+			}
+		?>
+	</body>
+</html>
